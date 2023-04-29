2f359a58db2b96b5e100e41d3ecb4fdd977c0b54
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-29 17:53:09 -0300

    Refatorando os códigos SCSS para melhorar a estilização da página na versão mobile.

--- public/js/classes/conversor.js
+++ public/js/classes/conversor.js
@@ -1,24 +1,24 @@
-export default class Conversor {
+// export default class Conversor {
 
-    decimalParaOutraBase(numero, base) {
-        const resto = numero % base;
-        const quociente = Math.trunc(numero / base);
+//     decimalParaOutraBase(numero, base) {
+//         const resto = numero % base;
+//         const quociente = Math.trunc(numero / base);
 
-        numeroConvertido.push(resto);
+//         numeroConvertido.push(resto);
 
-        if(quociente <= 1) {
-            numeroConvertido.push(quociente);
-            Conversor.imprimeResposta(numeroConvertido.reverse().join(""));
+//         if(quociente <= 1) {
+//             numeroConvertido.push(quociente);
+//             Conversor.imprimeResposta(numeroConvertido.reverse().join(""));
 
-            return;
-        }
+//             return;
+//         }
         
-        this.decimalParaOutraBase(quociente, base);
-    }
+//         this.decimalParaOutraBase(quociente, base);
+//     }
 
-    static imprimeResposta(numeroConvertido) {
-        $("[data-resposta]").val(numeroConvertido);
-    }
-}
+//     static imprimeResposta(numeroConvertido) {
+//         $("[data-resposta]").val(numeroConvertido);
+//     }
+// }
 
-const numeroConvertido = [];
+// const numeroConvertido = [];
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-import Conversor from "./classes/conversor.js";
+// import Conversor from "./classes/conversor.js";
 
-const conversor = new Conversor();
-const inputNumero = $("[data-numero]");
+// const conversor = new Conversor();
+// const inputNumero = $("[data-numero]");
 
-inputNumero.blur(function() {
-    const numeroParaConverter = $(this).val();
+// inputNumero.blur(function() {
+//     const numeroParaConverter = $(this).val();
 
-    conversor.decimalParaOutraBase(numeroParaConverter, 2);
-});
+//     conversor.decimalParaOutraBase(numeroParaConverter, 2);
+// });
