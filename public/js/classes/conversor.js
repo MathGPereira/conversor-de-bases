3154683e9a9bff70e449619aa76c6491cb30413a
Matheus Pereira <matheusgp.mto@outlook.com>
2023-04-30 15:18:37 -0300

    Criando uma mock API com o db.json, resolvendo bug de conversão para bases maiores que 10 e refatorando a classe Conversor para suportar AJAX

--- public/js/classes/conversor.js
+++ public/js/classes/conversor.js
@@ -1,25 +1,19 @@
 export default class Conversor {
 
-    decimalParaOutraBase(numero, base) {
+    async decimalParaOutraBase(numero, base) {
         const resto = numero % base;
         const quociente = Math.trunc(numero / base);
 
-        numeroConvertido.push(resto);
+        Conversor.validaResto(resto, numeroConvertido)
 
-        if(quociente <= 1) {
-            numeroConvertido.push(quociente);
-            numeroConvertido.reverse()[0] === 0 ? numeroConvertido.reverse().pop() : numeroConvertido.reverse();
-
-            Conversor.imprimeResposta(numeroConvertido.reverse().join(""));
-            numeroConvertido = [];
-
+        if(Conversor.validaQuociente(quociente, numeroConvertido)) {
             return;
         }
-        
-        this.decimalParaOutraBase(quociente, base);
+
+        await this.decimalParaOutraBase(quociente, base);
     }
 
-    outraBaseParaDecimal(numero, baseOriginal, baseFinal) {
+    async outraBaseParaDecimal(numero, baseOriginal, baseFinal) {
         let resultado = 0;
         const listaDeAlgarismos = numero.split("").reverse();
 
@@ -27,7 +21,37 @@ export default class Conversor {
             resultado += Number(numero) * (baseOriginal ** indice);
         });
 
-        this.decimalParaOutraBase(resultado, baseFinal);
+        await this.decimalParaOutraBase(resultado, baseFinal);
+    }
+
+    static async conectaComAPI() {
+        const url = "http://localhost:3000/restoParaHexadecimal";
+        const requisicao = await fetch(url);
+
+        return requisicao.json();
+    }
+
+    static async validaResto(resto, numeroConvertido) {
+        if(resto >= 10) {
+            const getBancoDeDados = await this.conectaComAPI();
+            const restoString = `${resto}`;
+
+            numeroConvertido.push(getBancoDeDados[0][restoString]);            
+        }else {
+            numeroConvertido.push(resto);
+        }
+    }
+
+    static validaQuociente(quociente, numeroConvertido) {
+        if(quociente <= 1) {
+            numeroConvertido.push(quociente);
+            numeroConvertido.reverse()[0] === 0 ? numeroConvertido.reverse().pop() : numeroConvertido.reverse();
+
+            this.imprimeResposta(numeroConvertido.reverse().join(""));
+            numeroConvertido = [];
+
+            return true;
+        }
     }
 
     static imprimeResposta(numeroConvertido) {
