e4089f6b72a0718946f5d2626258ba43cf47c8c3
Matheus Pereira <matheusgp.mto@outlook.com>
2023-05-01 16:52:03 -0300

    Refatorando o método outraBaseParaDecimal da ckasse Conversor

--- public/js/classes/conversor.js
+++ public/js/classes/conversor.js
@@ -1,57 +1,30 @@
 export default class Conversor {
 
     async decimalParaOutraBase(numero, base) {
-        const resto = numero % base;
-        const quociente = Math.trunc(numero / base);
-
-        Conversor.validaResto(resto, numeroConvertido)
-
-        if(Conversor.validaQuociente(quociente, numeroConvertido)) {
-            return;
-        }
-
-        await this.decimalParaOutraBase(quociente, base);
+        
     }
 
     async outraBaseParaDecimal(numero, baseOriginal, baseFinal) {
-        let resultado = 0;
-        const listaDeAlgarismos = numero.split("").reverse();
+        let numeroBaseDez = 0;
+        const listaAlgarismosConvertidos = [];
+        const listaAlgarismos = numero.split("");
+        const bancoDeDados = await Conversor.conectaComAPI("http://localhost:3000/BaseMaiorQueDecimalParaResto");
 
-        listaDeAlgarismos.forEach((numero, indice) => {
-            resultado += Number(numero) * (baseOriginal ** indice);
+        listaAlgarismos.map(algarismo => {
+            Object.keys(bancoDeDados[0]).find(item => algarismo === item) ? listaAlgarismosConvertidos.push(bancoDeDados[0][algarismo]) : listaAlgarismosConvertidos.push(algarismo);
         });
 
-        await this.decimalParaOutraBase(resultado, baseFinal);
+        listaAlgarismosConvertidos.reverse().forEach((algarismoConvertido, indice) => {
+            numeroBaseDez += algarismoConvertido * (baseOriginal ** indice);
+        });
+
+        this.decimalParaOutraBase(numeroBaseDez, baseFinal);
     }
 
-    static async conectaComAPI() {
-        const url = "http://localhost:3000/restoParaHexadecimal";
+    static async conectaComAPI(url) {
         const requisicao = await fetch(url);
 
         return requisicao.json();
-    }
-
-    static async validaResto(resto, numeroConvertido) {
-        if(resto >= 10) {
-            const getBancoDeDados = await this.conectaComAPI();
-            const restoString = `${resto}`;
-
-            numeroConvertido.push(getBancoDeDados[0][restoString]);            
-        }else {
-            numeroConvertido.push(resto);
-        }
-    }
-
-    static validaQuociente(quociente, numeroConvertido) {
-        if(quociente <= 1) {
-            numeroConvertido.push(quociente);
-            numeroConvertido.reverse()[0] === 0 ? numeroConvertido.reverse().pop() : numeroConvertido.reverse();
-
-            this.imprimeResposta(numeroConvertido.reverse().join(""));
-            numeroConvertido = [];
-
-            return true;
-        }
     }
 
     static imprimeResposta(numeroConvertido) {
